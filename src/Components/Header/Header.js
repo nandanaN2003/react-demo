170c1bdae2bd5c5a125b55211e9708648a50ef7f
Nandana Babu <nandanan260@gmail.com>
2025-02-24 22:08:36 +0530

    Header has changed to some data

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,11 @@ function Header()
     return(
     <>
     <div className="nav">
-        <h1>Home</h1>
+        <h1>Home new</h1>
+        <h1>About Us</h1>
+        <h1>Contact US</h1>
+        <h1>Login</h1>
+        <h1>Home new</h1>
         <h1>About Us</h1>
         <h1>Contact US</h1>
         <h1>Login</h1>
